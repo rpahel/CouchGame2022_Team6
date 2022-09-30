1ad1ea932a3d961a43d634691b90c58c6a661630
Julien <julien.aberton@edu.devinci.fr>
2022-09-30 14:29:49 +0200

    start projectile

---
+++ Assets/Scripts/Julien/AutoDestroy.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoDestroy : MonoBehaviour
+{
+    public void Destroy(float time)
+    {
+        Destroy(this, time);
+    }
+    
+    
+
+}
--- Assets/Scripts/Julien/ShootProjectile.cs
+++ Assets/Scripts/Julien/ShootProjectile.cs
@@ -1,29 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-[RequireComponent(typeof(Rigidbody2D))]
+using UnityEngine.InputSystem;
 public class ShootProjectile : MonoBehaviour
 {
     [Header("Options Projectile")]
-    [SerializeField] private GameObject projectile;
+    [SerializeField] private GameObject projectilePrefab;
     [SerializeField] private bool canCreatePlatform;
     [SerializeField, Range(0, 1)] private float gravityScale;
     [SerializeField, Range(1, 100)] private float mass;
+    [SerializeField] private float lifetime;
     
     private bool _canShoot;
-
+    private Vector2 mousePos;
+    
     private Rigidbody2D _rb;
 
-    void Awake()
+    void OnShoot()
     {
-        _rb = gameObject.GetComponent<Rigidbody2D>();
+        Debug.Log("shoot");
+        var projectile = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+        _rb = projectile.GetComponent<Rigidbody2D>();
         _rb.mass = mass;
         _rb.gravityScale = gravityScale;
+        projectile.GetComponent<AutoDestroy>().Destroy((lifetime));
     }
 
-    void Shoot()
+    void Update()
     {
-        
+        mousePos = Input.mousePosition;
+    }
+    
+    void OnDrawGizmos()
+    {
+        Gizmos.DrawLine(transform.position, (mousePos - (Vector2)transform.position).normalized);
     }
 }
