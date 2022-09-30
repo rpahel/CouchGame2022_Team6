1e97fdee65a7c5ddaad7b1f55d11f8649ab30876
rpahel <raphael.dacosta.pro@gmail.com>
2022-09-30 18:30:47 +0200

    Viser + clique pour manger le cube

---
+++ Assets/Scripts/Raphael/Raf_CubeMangeable.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raf_CubeMangeable : MonoBehaviour
+{
+    public void GetManged()
+    {
+        Destroy(gameObject);
+    }
+}
--- Assets/Scripts/Raphael/Raf_PLayer.cs
+++ Assets/Scripts/Raphael/Raf_PLayer.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class Raf_PLayer : MonoBehaviour
 {
     private CharacterController chaC;
     public Transform pointeur;
+    public Transform pointeurBase;
+    public float reach = 1f;
+    private Vector2 mousePos2;
+    private Vector2 mousePos3;
+    private Vector2 playerToMouse;
+    private float angle;
 
     private void Awake()
     {
@@ -14,8 +21,31 @@ public class Raf_PLayer : MonoBehaviour
 
     private void Update()
     {
-        chaC.SimpleMove(Vector3.right * Input.GetAxis("Horizontal") * 10f);
-        pointeur.Rotate(new Vector3(0, 0, Input.GetAxis("Vertical")));
+        mousePos2 = Input.mousePosition;
 
+        chaC.SimpleMove(Vector3.right * Input.GetAxis("Horizontal") * 10f);
+
+        mousePos3 = Camera.main.ScreenToWorldPoint(new Vector3(mousePos2.x, mousePos2.y, -Camera.main.transform.position.z));
+        playerToMouse = (mousePos3 - (Vector2)pointeur.position).normalized;
+        angle = Mathf.Atan2(playerToMouse.y, playerToMouse.x);
+        pointeur.rotation = Quaternion.Euler(0, 0, Mathf.Rad2Deg * angle);
+
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(pointeurBase.position, playerToMouse, out hit, reach))
+            {
+                print(hit.collider.gameObject.name);
+                if (hit.collider.CompareTag("CubeMangeable"))
+                {
+                    Raf_CubeMangeable cubeMangeable;
+                    if (hit.collider.TryGetComponent<Raf_CubeMangeable>(out cubeMangeable))
+                        cubeMangeable.GetManged();
+                    else
+                        print("Pas de Raf_CubeMangeable dans le cube visé.");
+                }
+            }
+        }
     }
 }
