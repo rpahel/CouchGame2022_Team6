0aa91e9acb43222660a0b4b574b104a0be519eca
Julien <julien.aberton@edu.devinci.fr>
2022-10-12 10:09:49 +0200

    Update ScaleEat.cs

--- Assets/Scripts/Maxime/ScaleEat.cs
+++ Assets/Scripts/Maxime/ScaleEat.cs
@@ -6,36 +6,30 @@ public class ScaleEat : MonoBehaviour
 {
     public enum SwitchSizeSkin
     {
-        little,
-        average,
-        big
+        Little,
+        Medium,
+        Big
     }  
 
     public SwitchSizeSkin switchSkin;
-    public float NbEaten = 200f;
+    public float NbEaten = 1;
     public Vector3 scaler = new Vector3(1,1,1);
     public Mesh meshLittle;
     public Mesh meshAverage;
     public Mesh meshBig;
     public Mesh CurrentMesh;
-    private Mesh _meshFilterGo;
-
-    private DashCardinalDirection dashCardinalDirection;
-    // public bool squashed;
+    private MeshFilter _meshFilterGo;
 
     private void InitializedSize()
     {
-        switchSkin = SwitchSizeSkin.little;
-        dashCardinalDirection = GetComponent<DashCardinalDirection>();
+        switchSkin = SwitchSizeSkin.Little;
     }
     private void Start()
     {
-        _meshFilterGo = this.transform.GetChild(0).GetComponent<MeshFilter>().mesh;
-        Debug.Log(_meshFilterGo);
-        CurrentMesh = _meshFilterGo;
+        _meshFilterGo = this.transform.GetChild(0).GetComponent<MeshFilter>();
+        CurrentMesh = _meshFilterGo.mesh;
 
     }
-    // Update is called once per frame
     void Update()
     {
        scaleEat();
@@ -44,53 +38,28 @@ public class ScaleEat : MonoBehaviour
     void scaleEat()
     {
         transform.localScale = scaler;
-        NbEaten = Mathf.Clamp(NbEaten, 1, 300);
+        NbEaten = Mathf.Clamp(NbEaten, 0.2f, 1f);
 
         //juste pour sa soit smooth
-        scaler.y = Mathf.Lerp(scaler.y, NbEaten * 0.005f, .03f);
-        scaler.x = Mathf.Lerp(scaler.x, NbEaten * 0.005f, .03f);
-        scaler.z = Mathf.Lerp(scaler.z, NbEaten * 0.005f, .03f);
+        scaler.y = Mathf.Lerp(scaler.y, NbEaten, .03f); 
+        scaler.x = Mathf.Lerp(scaler.x, NbEaten, .03f);
+        scaler.z = Mathf.Lerp(scaler.z, NbEaten, .03f);
 
-
-        //les different etat du player
-        switch (switchSkin)
+        if (NbEaten >= 0.7f)
         {
-            case SwitchSizeSkin.little:
-                InitializedSize();
-                CurrentMesh = meshLittle;
-                if (NbEaten >= 100)
-                {
-                    switchSkin = SwitchSizeSkin.average;
-                    _meshFilterGo = meshAverage;
-                }
-                break;
-            case SwitchSizeSkin.average:
-                CurrentMesh = meshAverage;
-                if (NbEaten < 100)
-                {
-                    switchSkin = SwitchSizeSkin.little;
-                    _meshFilterGo = meshLittle;
-                }
-                else if (NbEaten >= 200)
-                {
-                    switchSkin = SwitchSizeSkin.big;
-                    _meshFilterGo = meshBig;
-                }
-                break;
-            case SwitchSizeSkin.big:
-                CurrentMesh = meshBig;
+            switchSkin = SwitchSizeSkin.Big;
+            _meshFilterGo.mesh = meshBig;
             
-                if (NbEaten < 100)
-                {
-                    switchSkin = SwitchSizeSkin.little;
-                    _meshFilterGo = meshLittle;
-                }
-                else if (NbEaten < 200)
-                {
-                    switchSkin = SwitchSizeSkin.average;
-                    _meshFilterGo = meshAverage;
-                }
-                break;
+        }
+        else if (NbEaten <= 0.3f)
+        {
+            switchSkin = SwitchSizeSkin.Little;
+            _meshFilterGo.mesh = meshLittle;
+        }
+        else
+        {
+            switchSkin = SwitchSizeSkin.Medium;
+            _meshFilterGo.mesh = meshAverage;
         }
     }
 }
