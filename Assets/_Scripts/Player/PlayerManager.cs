8bbaa76e0ba995f5c1ce7ac6ec865a79bd3822d5
MaximeBritto <90254429+MaximeBritto@users.noreply.github.com>
2022-12-05 10:28:27 +0100

    flipFonctionnel

--- Assets/_Scripts/Player/PlayerManager.cs
+++ Assets/_Scripts/Player/PlayerManager.cs
@@ -422,7 +422,7 @@ public class PlayerManager : MonoBehaviour
     void Flip()
     {
         Vector3 currentScale = gameObject.transform.localScale;
-        currentScale.x *= -1;
+        currentScale.x *= 1;
         gameObject.transform.localScale = currentScale;
 
         facingRight = !facingRight;
